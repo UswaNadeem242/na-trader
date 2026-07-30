246e31e2e3101df701731ea0c0e2718739126b3f
UswaNadeem242 <zazunala7@gmail.com>
2026-07-30 23:44:38 +0500

    fix

--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -15,7 +15,7 @@ import ser2 from "@/assets/images/PNG/car_with_flash.png"
 import ser3 from "@/assets/images/PNG/car_eco.png"
 import ser4 from "@/assets/images/PNG/accumulator.png"
 import ser5 from "@/assets/images/PNG/battery_full.png"
-import ser6 from "@/assets/images/car_with_plug.png"
+import ser6 from "@/assets/images/PNG/car_with_plug.png"
 export const menuItems = [
   {
     title: "Home",
